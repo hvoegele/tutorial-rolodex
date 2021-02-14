e4513507d6dfd10cc7b5be4ba87d7ef249c678bc
hvoegele <hunterlvoegele@gmail.com>
2021-02-13 22:34:55 -0500

    Initializing project with minor changes to HTML.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() { 
+    super();
+
+    this.state = {
+      string: 'Hello Hunter'
+    }
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({string: "Hello Yihua"})}> Change Text </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
